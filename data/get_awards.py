d48b303e6fdd56d1589ce407259a46a1e10124cc
Mickus Timothee <mickus@lx2-400-27713.ad.helsinki.fi>
2025-07-10 09:56:38 +0300

    script update

--- data/get_awards.py
+++ data/get_awards.py
@@ -8,6 +8,15 @@ import tqdm
 def try_get(obj, child):
 	return obj.find(child).text if obj.find(child) else None
 
+def maybe_pdf_url(obj):
+	maybe_url = try_get(obj, 'url')
+	if maybe_url and not 'http' in maybe_url:
+		return f'https://aclanthology.org/{maybe_url}.pdf', True
+	elif 'http' in maybe_url:
+		return maybe_url, False
+	else:
+		return None, False
+
 records = []
 files = list(pathlib.Path('acl-anthology/data/xml').glob('*.xml'))
 for file in tqdm.tqdm(files):
@@ -15,10 +24,13 @@ for file in tqdm.tqdm(files):
 		soup = BeautifulSoup(istr.read(), 'xml')
 	for paper in soup.find_all('paper'):
 		if paper.find('award'):
+			url, extracted = maybe_pdf_url(paper)
 			records.append({
 				'title': try_get(paper, 'title'),
 				'abstract': try_get(paper, 'abstract'),
 				'doi': try_get(paper, 'doi'),
+				'url': url,
+				'extracted': extracted,
 				'datafile': file.name,
 				'authors': [
 					{
